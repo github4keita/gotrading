d3fee56a92b57d034f4b26163909d8cee252b2db
Keita Matsuo <matsuokeita@team-lab.com>
2019-03-10 06:34:26 +0900

    signal_events関係のモデルとメソッド作成

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"gotrading/app/controllers"
-	"gotrading/config"
-	"gotrading/utils"
+	"fmt"
+	"gotrading/app/models"
+	"time"
 )
 
+// import (
+// 	"gotrading/app/controllers"
+// 	"gotrading/config"
+// 	"gotrading/utils"
+// )
+
+// func main() {
+// 	utils.LoggingSetting(config.Config.LogFile)
+// 	controllers.StreamIngestionData()
+// 	controllers.StartWebServer()
+// }
+
 func main() {
-	utils.LoggingSetting(config.Config.LogFile)
-	controllers.StreamIngestionData()
-	controllers.StartWebServer()
+	s := models.NewSignalEvents()
+	df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
+	c1 := df.Candles[0]
+	c2 := df.Candles[5]
+	s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
+	s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
+	fmt.Println(s.CollectAfter(time.Now().UTC()))
+	fmt.Println(s.CollectAfter(c1.Time))
 }
