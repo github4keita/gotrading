02e9b1b810faab5354a2fa53a443574f5bb16a24
Keita Matsuo <matsuokeita@team-lab.com>
2019-03-12 07:14:03 +0900

    EMAによるバックテスト

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -200,6 +200,20 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 		df.AddHv(period3)
 	}
 
+	events := r.URL.Query().Get("events")
+	if events != "" {
+		if config.Config.BackTest {
+			p, p1, p2 := df.OptimizeEma()
+			log.Println(p, p1, p2)
+			if p > 0 {
+				df.Events = df.BackTestEma(p1, p2)
+			}
+		} else {
+			firstTime := df.Candles[0].Time
+			df.AddEvents(firstTime)
+		}
+	}
+
 	js, err := json.Marshal(df)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"gotrading/app/models"
-	"time"
+	"gotrading/app/controllers"
+	"gotrading/config"
+	"gotrading/utils"
+	"log"
 )
 
-// import (
-// 	"gotrading/app/controllers"
-// 	"gotrading/config"
-// 	"gotrading/utils"
-// )
-
-// func main() {
-// 	utils.LoggingSetting(config.Config.LogFile)
-// 	controllers.StreamIngestionData()
-// 	controllers.StartWebServer()
-// }
-
 func main() {
-	s := models.NewSignalEvents()
-	df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
-	c1 := df.Candles[0]
-	c2 := df.Candles[5]
-	s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
-	s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
-	fmt.Println(s.CollectAfter(time.Now().UTC()))
-	fmt.Println(s.CollectAfter(c1.Time))
+	utils.LoggingSetting(config.Config.LogFile)
+	// controllers.StreamIngestionData()
+	log.Println(controllers.StartWebServer())
 }
